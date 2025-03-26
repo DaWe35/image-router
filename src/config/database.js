13a9f9b47d417a53fc4a2a89a76010e99b61bdb3
DaWe <willteam35@gmail.com>
2025-03-26 03:37:52 +0100

    remove bs

--- src/config/database.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'image_router',
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  logging: process.env.NODE_ENV === 'development' ? console.log : false
-} 
