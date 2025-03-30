fd50b096809d6e40974c38ec7e85260e6b5b845a
DaWe <willteam35@gmail.com>
2025-03-30 22:47:35 +0200

    use env port in log

--- src/index.js
+++ src/index.js
@@ -57,5 +57,5 @@ process.on('SIGINT', async () => {
 })
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${process.env.PORT}`)
 }) 
