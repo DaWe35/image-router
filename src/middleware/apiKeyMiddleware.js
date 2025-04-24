9572a005a207bdc33b3f4641aaee3c5678808727
DaWe <willteam35@gmail.com>
2025-04-24 10:21:03 +0200

    test on prod lol I cant reproduce the issue, but a bot is currently doing this for me on prod

--- src/middleware/apiKeyMiddleware.js
+++ src/middleware/apiKeyMiddleware.js
@@ -196,6 +196,7 @@ export const logApiUsage = async (req, res, next) => {
                         error: res.statusCode !== 200 ? JSON.stringify(data?.errorResponse) : null
                     }
                 })
+                console.log(JSON.stringify(data?.errorResponse))
 
                 // If the request failed, refund the credits
                 if (res.statusCode !== 200) {
