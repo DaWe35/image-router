1c2b42fc943fa30150f7c53eab39b64c471e5f21
DaWe <willteam35@gmail.com>
2025-05-05 14:58:24 +0200

    remove dall-e-2 editing

--- src/shared/models/openai/dall-e-2.js
+++ src/shared/models/openai/dall-e-2.js
@@ -1,4 +1,3 @@
-import fs from 'fs'
 import { PRICING_TYPES } from '../../PricingScheme.js'
 
 class DallE2 {
@@ -11,9 +10,7 @@ class DallE2 {
           type: PRICING_TYPES.FIXED,
           value: 0.02,
         },
-        applyQuality: this.applyQuality,
-        applyImage: this.applyImage,
-        applyMask: this.applyMask
+        applyQuality: this.applyQuality
       }],
       arena_score: 714,
       examples: [
@@ -30,30 +27,6 @@ class DallE2 {
 
   applyQuality(params) {
     delete params.quality // Dall-E 2 does not support quality, even if their docs say it does. Default quality is standard, no other options available..
-    return params
-  }
-
-  applyImage(params) {
-    if (Array.isArray(params.files.image)) {
-      if (params.files.image.length !== 1) {
-        throw new Error('DALL-E 2 does not support multiple images. Please provide only one image for editing.')
-      }
-      // Exactly one image provided in array
-      params.image = fs.createReadStream(params.files.image[0].path)
-    } else {
-      // For a single image provided as an object
-      params.image = fs.createReadStream(params.files.image.path)
-    }
-
-    console.log('params.image', params.image)
-
-    delete params.files.image
-    return params
-  }
-
-  applyMask(params) {
-    params.mask = fs.createReadStream(params.files.mask.path)
-    delete params.files.mask
     return params
   }
 }
