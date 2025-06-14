d3c0408eb763b68446a2705a87ca85a9f8311c3d
DaWe <willteam35@gmail.com>
2025-06-14 22:05:22 +0200

    add kling-1.6-standard model

---
+++ src/shared/videoModels/kwaivgi/kling-1.6-standard.js
@@ -0,0 +1,26 @@
+import { PRICING_TYPES } from '../../PricingScheme.js'
+
+class Kling16Standard {
+  constructor() {
+    this.data = {
+      id: 'kwaivgi/kling-1.6-standard',
+      providers: [{
+        id: 'replicate',
+        model_name: 'kwaivgi/kling-v1.6-standard',
+        pricing: {
+          type: PRICING_TYPES.FIXED,
+          value: 0.25, // price per video on Replicate
+        },
+      }],
+      arena_score: 1029,
+      release_date: '2024-12-19',
+      examples: []
+    }
+  }
+
+  getData() {
+    return this.data
+  }
+}
+
+export default Kling16Standard 
