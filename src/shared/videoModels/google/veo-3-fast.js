7732951d0e3bd2e6745c8cfaabfe168a9513c839
DaWe <willteam35@gmail.com>
2025-07-10 21:54:32 +0200

    update Veo3Fast pricing

--- src/shared/videoModels/google/veo-3-fast.js
+++ src/shared/videoModels/google/veo-3-fast.js
@@ -5,11 +5,18 @@ class Veo3Fast {
     this.data = {
       id: 'google/veo-3-fast',
       providers: [{
-        id: 'wavespeed',
-        model_name: 'google/veo3-fast',
+        id: 'replicate',
+        model_name: 'google/veo-3-fast',
         pricing: {
           type: PRICING_TYPES.FIXED,
-          value: 3,
+          value: 3.2,
+        },
+      }, {
+        id: 'wavespeed',
+        model_name: 'google/veo3-fast', // no audio ?
+        pricing: {
+          type: PRICING_TYPES.FIXED,
+          value: 3.2,
         },
       }],
       release_date: '2025-06-12'
