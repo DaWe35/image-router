f234b2f2a55538eab24bf5e9f73d152ac790de8e
DaWe <willteam35@gmail.com>
2025-07-16 10:46:52 +0200

    Increase IP request limit

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ const generalLimiter = rateLimit({
 
 const ipLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
-    max: 100, // limit each IP to 100 requests per minute
+    max: 120, // limit each IP to 100 requests per minute
     keyGenerator: ipKeyGenerator, // Use custom key generator
     message: {
         error: {
