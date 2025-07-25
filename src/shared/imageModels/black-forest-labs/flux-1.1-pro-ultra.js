59b8ede65de88a0ae54dca9deecd58618e2417e8
DaWe <willteam35@gmail.com>
2025-07-25 12:53:47 +0200

    update image model pricing

--- src/shared/imageModels/black-forest-labs/flux-1.1-pro-ultra.js
+++ src/shared/imageModels/black-forest-labs/flux-1.1-pro-ultra.js
@@ -1,17 +1,28 @@
 import { PRICING_TYPES } from '../../PricingScheme.js'
+import { postCalcSimple } from '../../../services/imageHelpers.js'
 
 class Flux11ProUltra {
   constructor() {
     this.data = {
       id: 'black-forest-labs/flux-1.1-pro-ultra',
-      providers: [{
-        id: 'replicate',
-        model_name: 'black-forest-labs/flux-1.1-pro-ultra',
-        pricing: {
-          type: PRICING_TYPES.FIXED,
-          value: 0.06,
+      providers: [
+        {
+          id: 'runware',
+          model_name: 'bfl:2@2',
+          pricing: {
+            type: PRICING_TYPES.POST_GENERATION,
+            postCalcFunction: postCalcSimple,
+            value: 0.06,
+          },
+        }, {
+          id: 'replicate',
+          model_name: 'black-forest-labs/flux-1.1-pro-ultra',
+          pricing: {
+            type: PRICING_TYPES.FIXED,
+            value: 0.06,
+          }
         }
-      }],
+      ],
       arena_score: 1094,
       release_date: '2024-11-06',
       examples: [
