e7f3743a8a839dd157c20efe62c4e1960f19df1c
DaWe <willteam35@gmail.com>
2025-10-08 21:25:59 +0200

    add QwenImageEditPlus and QwenSnofs models, remove hardcoded model reference in QwenImageEdit

---
+++ src/shared/imageModels/qwen/qwen-image-edit-plus.js
@@ -0,0 +1,32 @@
+import { PRICING_TYPES } from '../../PricingScheme.js'
+import { postCalcSimple, processSingleOrMultipleFiles } from '../../../services/imageHelpers.js'
+import { applyReferenceImages } from '../../applyImage.js'
+
+class QwenImageEditPlus {
+  constructor() {
+    this.data = {
+      id: 'qwen/qwen-image-edit-plus',
+      providers: [
+        {
+          id: 'runware',
+          model_name: 'runware:108@22',
+          pricing: {
+            type: PRICING_TYPES.POST_GENERATION,
+            postCalcFunction: postCalcSimple,
+            value: 0.0083
+          },
+          applyImage: applyReferenceImages
+        }
+      ],
+      arena_score: 1092,
+      release_date: '2025-08-18'
+
+    }
+  }
+
+  getData() {
+    return this.data
+  }
+}
+
+export default QwenImageEditPlus
--- src/shared/imageModels/qwen/qwen-snofs.js
+++ src/shared/imageModels/qwen/qwen-snofs.js
@@ -1,43 +1,48 @@
 import { PRICING_TYPES } from '../../PricingScheme.js'
 import { postCalcSimple, processSingleOrMultipleFiles } from '../../../services/imageHelpers.js'
 
-class QwenImageEdit {
+class QwenSnofs {
   constructor() {
     this.data = {
-      id: 'qwen/qwen-image-edit',
+      id: 'qwen/qwen-snofs',
       providers: [
         {
           id: 'runware',
-          model_name: 'runware:108@20',
+          model_name: 'pasaranax:1972981@2233198',
           pricing: {
             type: PRICING_TYPES.POST_GENERATION,
             postCalcFunction: postCalcSimple,
             range: {
-              min: 0.0032,
-              average: 0.0058,
-              max: 0.0096
+              min: 0.0058,
+              average: 0.007,
+              max: 0.0122
             }
           },
           applyQuality: this.applyQualityRunware,
           applyImage: this.applyImageRunware
         }
       ],
-      arena_score: 1092,
-      release_date: '2025-08-18'
-
+      arena_score: 1073,
+      release_date: '2025-08-01',
+      examples: [
+        {
+          image: '/model-examples/qwen-image-2025-08-07T13-00-25-337Z.webp'
+        }
+      ]
     }
   }
 
   async applyImageRunware(params) {
     params.referenceImages = await processSingleOrMultipleFiles(params.files.image, 'datauri')
+    params.model = 'runware:108@20'
     return params
   }
 
   applyQualityRunware(params) {
     const qualitySteps = {
-      low: 8,
-      medium: 15,
-      high: 30
+      low: 20,
+      medium: 25,
+      high: 40
     }
     params.steps = qualitySteps[params.quality] ?? qualitySteps['medium']
     delete params.quality
@@ -49,4 +54,4 @@ class QwenImageEdit {
   }
 }
 
-export default QwenImageEdit
+export default QwenSnofs
