28985a3bda499b11f2b7eb3660d2516f5b8c95d4
DaWe <willteam35@gmail.com>
2025-12-16 22:00:08 +0100

    Add Flux2Max image model with pricing configuration and processing methods

---
+++ src/shared/imageModels/black-forest-labs/FLUX-2-max.js
@@ -0,0 +1,65 @@
+import { PRICING_TYPES } from '../../PricingScheme.js'
+import { postCalcSimple } from '../../../services/imageHelpers.js'
+
+export default class Flux2Max {
+  constructor() {
+    this.data = {
+      id: 'black-forest-labs/FLUX-2-max',
+      providers: [
+        {
+          id: 'runware',
+          model_name: 'bfl:7@1',
+          pricing: {
+            type: PRICING_TYPES.POST_GENERATION,
+            postCalcFunction: postCalcSimple,
+            range: {
+              min: 0.07,
+              average: 0.07,
+              max: 0.25
+            }
+          },
+          applyImage: this.applyImage,
+          applyMask: this.applyMask,
+          applyQuality: this.applyQuality,
+        }
+      ],
+      release_date: '2025-12-16',
+    }
+  }
+
+  async applyImage(params) {
+    params.seedImage = await processSingleFile(params.files.image, 'datauri')
+    delete params.files.image
+
+    if (!params.size || params.size === 'auto') {
+      const dimensions = await calculateRunwareDimensions(
+        params.seedImage,
+        { minPixels: undefined, maxPixels: undefined, minDimension: 128, maxDimension: 2048, pixelStep: 64 }
+      )
+      params.size = `${dimensions.width}x${dimensions.height}`
+    }
+    
+    return params
+  }
+
+  async applyMask(params) {
+    params.maskImage = await processSingleFile(params.files.mask, 'datauri')
+    delete params.files.mask
+    return params
+  }
+
+  applyQuality(params) {
+    const qualitySteps = {
+      low: 15,
+      medium: 20,
+      high: 45
+    }
+    params.steps = qualitySteps[params.quality] ?? qualitySteps['medium']
+    delete params.quality
+    return params
+  }
+
+  getData() {
+    return this.data
+  }
+}
