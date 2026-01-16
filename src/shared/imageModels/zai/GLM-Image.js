2e21240dfbfa20a6e1d57f0c44838e79b832efff
DaWe <willteam35@gmail.com>
2026-01-16 16:59:50 +0100

    add more sizes to GLM Image

--- src/shared/imageModels/zai/GLM-Image.js
+++ src/shared/imageModels/zai/GLM-Image.js
@@ -5,6 +5,9 @@ export default class {
     this.data = {
       id: 'zai/glm-image',
       sizes: [
+        '1024x1024',
+        '2048x2048',
+        '512x512',
         '1280x1280',
         '1568x1056',
         '1056x1568',
@@ -12,6 +15,10 @@ export default class {
         '1088x1472',
         '1728x960',
         '960x1728',
+        '1024x768',
+        '768x1024',
+        '1024x576',
+        '576x1024',
       ],
       providers: [
         {
