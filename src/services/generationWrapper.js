31e5bc98b4c7d9b87acd9e13fa46e6ef4240ad56
DaWe <willteam35@gmail.com>
2026-01-16 22:39:05 +0100

    Add new retryable error message for Google provider in generationWrapper.js

--- src/services/generationWrapper.js
+++ src/services/generationWrapper.js
@@ -41,7 +41,8 @@ const RETRYABLE_ERRORS = [
   `Processing (Stuck?)`,
   `Internal error encountered.`,
   `ByteDance responded with an error. The provider returned the . Additional information below.`, // usually server overload
-  'The provider "Google" returned an invalid response. Additional information: No content data returned from Google'
+  'The provider "Google" returned an invalid response. Additional information: No content data returned from Google',
+  'High concurrency usage of this API, please reduce concurrency or contact customer service to increase limits',
 ]
 
 function isRetryableError(error) {
