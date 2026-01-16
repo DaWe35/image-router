6b0045f3d74ec1150461593a6c818e2e96060c36
DaWe <willteam35@gmail.com>
2026-01-16 19:45:38 +0100

    switch provider for Z-Image FREE

--- src/shared/imageModels/tongyi-mai/Z-Image-Turbo-FREE.js
+++ src/shared/imageModels/tongyi-mai/Z-Image-Turbo-FREE.js
@@ -7,30 +7,16 @@ export default class {
       id: 'Tongyi-MAI/Z-Image-Turbo:free',
       providers: [
         {
-          id: 'runware',
-          model_name: 'runware:z-image@turbo',
+          id: 'chutes',
+          model_name: 'z-image-turbo',
           pricing: {
             type: PRICING_TYPES.FIXED,
             value: 0
           },
-          applyQuality: this.applyQualityRunware
         },
       ],
       release_date: '2025-11-25'
     }
-  }
-
-  applyQualityRunware(params) {
-    const qualitySteps = {
-      low: 4,
-      medium: 8
-    }
-    if (params.quality === 'high') {
-      throw new Error(`Free models only supports 'medium' and 'low' quality. Please use the paid model 'Tongyi-MAI/Z-Image-Turbo' for higher quality.`)
-    }
-    params.steps = qualitySteps[params.quality] ?? qualitySteps['medium']
-    delete params.quality
-    return params
   }
 
   getData() {
