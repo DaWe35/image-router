bf8356256224a8706f16717236fc5589e5b47768
DaWe <willteam35@gmail.com>
2026-01-16 16:54:23 +0100

    Update pricing value and comment out non-functional image size

--- src/shared/imageModels/runware/imagineart-1.5.js
+++ src/shared/imageModels/runware/imagineart-1.5.js
@@ -11,20 +11,20 @@ export default class {
         pricing: {
           type: PRICING_TYPES.POST_GENERATION,
           postCalcFunction: postCalcSimple,
-          value: 0.01
+          value: 0.03
         }
       }],
       release_date: '2025-11-14',
       sizes: [
         '2048x2048',
-        '2560x1440',
+        /* '2560x1440', // not working rn
         '1440x2560',
         '2304x1728',
         '1728x2304',
         '2472x824',
         '824x2472',
         '1728x1152',
-        '1152x1728'
+        '1152x1728' */
       ]
     }
   }
